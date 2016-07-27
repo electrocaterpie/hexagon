735b0728df1f03fb7585e44082dd800e5624df81
electrocaterpie <electrocaterpie@gmail.com>
2016-07-28 02:48:04 +0530

    Added Algorithm Initial logic to solve hexagon problem

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+function itDoesNothing() {
+    setHexaColor(2, 3, "#FF00FF");;
+}
+
+var defaultColor = "#888";
+
 function createDiv(_id, _class, _parent) {
     var newDiv = document.createElement("div");
     newDiv.setAttribute("id", _id);
@@ -8,11 +14,11 @@ function createDiv(_id, _class, _parent) {
     return newDiv;
 }
 
-function createHexagon(_hid, _text = "", _value="") {
+function createHexagon(_hid, _text = "", _value = "") {
     console.log("ENTER--createHexagon");
 
-    var colr = "#"+((1<<24)*Math.random()|0).toString(16);
-    //var colr = "#6C6";
+    //var colr = "#"+((1<<24)*Math.random()|0).toString(16);
+    var colr = defaultColor;
 
     var hexagon = createDiv("hexa_" + _hid, "hexagon", "mainDiv");
 
@@ -35,7 +41,20 @@ function createHexagon(_hid, _text = "", _value="") {
 
 var cells = [];
 
+function setHexaColor(y, x, colorStr = "#FFF") {
+    var hexId = "hexa_" + y + "_" + x;
+    var topId = "top_" + y + "_" + x;
+    var midId = "mid_" + y + "_" + x;
+    var btmId = "btm_" + y + "_" + x;
+
+    document.getElementById(topId).style.borderRightColor = colorStr;
+    document.getElementById(midId).style.backgroundColor = colorStr;
+    document.getElementById(btmId).style.borderLeftColor = colorStr;
+}
+
 function generateHexagons(W, H, valueArray) {
+
+    console.log("generateHexagons called with W: " + W + " H: " + H + " valueArray: " + valueArray);
 
     var startTop = -8;
     var diffTop = 54; //for eachrow
@@ -50,12 +69,14 @@ function generateHexagons(W, H, valueArray) {
         if (x == W) {
             y++;
             x = 0;
-            currTop += 2*diffTop;
+            currTop += 2 * diffTop;
         }
         cells.push({
+            index: i,
             cy: y,
             cx: x,
-            cost: valueArray[i]
+            cost: valueArray[i],
+            visited: false
         });
 
         var hexaId = y + "_" + x;
@@ -69,21 +90,100 @@ function generateHexagons(W, H, valueArray) {
             var cTop = currTop;
             var cLeft = x * diffLeft;
 
-            elemHexa.style.top = cTop+"px";
-            elemHexa.style.left = cLeft+"px";
+            elemHexa.style.top = cTop + "px";
+            elemHexa.style.left = cLeft + "px";
         } else {
             //neeche
             var cTop = currTop + diffTop;
             var cLeft = x * diffLeft;
-            elemHexa.style.top = cTop+"px";
-            elemHexa.style.left = cLeft+"px";
+            elemHexa.style.top = cTop + "px";
+            elemHexa.style.left = cLeft + "px";
         }
     }
 }
 
-generateHexagons(5, 3, [1, 2, 3,4,5,6,7,8,9,10,11,12,13,14,15])
-    //generateHexagons(5,3,[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])
+//generateHexagons(5,3,[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])
 
 //createHexagon("0_0","(0,0)");
 //createHexagon("0_1","(0,1)");
 //createHexagon("0_2","(0,2)");
+
+//--------------------------------------------
+
+var odd_oy = [-1, 0, 1, 1, 1, 1];
+var odd_ox = [0, 1, 1, 0, -1, -1];
+
+var even_oy = [-1, -1, 0, 1, 0, -1];
+var even_ox = [0, 1, 1, 0, -1, -1];
+
+var W = 5;
+var H = 3;
+var costArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+var gMaxCost = 0;
+
+function isSafeCord(cy, cx) {
+    if (cy < 0 || cy >= W || cx < 0 || cx >= H) return false;
+    return true;
+}
+
+function findCordIndex(cy,cx) {
+    for(i=0;i<(W*H);i++) {
+        if(cells[i].cy == cy && cells[i].cx == cx) return true;
+    }
+    return false;
+}
+
+function startAlgo(index, cost, depth) {
+
+    cost += cells[index].cost;
+
+    if (depth = 4 && cost > gMaxCost) {
+        gMaxCost = cost;
+        return;
+    }
+
+    var sy = cells[index].cy;
+    var sx = cells[index].cx;
+
+    cells[index].visited = true;
+
+    //sx even colm index
+    if (sx % 2 == 0) {
+        for (k = 0; k < 6; k++) {
+            var ty = sy + even_oy[k];
+            var tx = sx + even_ox[k];
+            var tIndex = findCordIndex(ty, tx);
+            startAlgo(tIndex, cost, depth + 1);
+        }
+
+    } else {
+        for (k = 0; k < 6; k++) {
+            var ty = sy + odd_oy[k];
+            var tx = sx + odd_ox[k];
+            var tIndex = findCordIndex(ty, tx);
+            startAlgo(tIndex, cost, depth + 1);
+        }
+
+    }
+    cells[index].visited = false;
+}
+
+function mainProg() {
+
+    generateHexagons(W, H, costArray);
+    //input done....
+
+    setHexaColor(2, 3, "#FF0000");
+   // setTimeout(itDoesNothing,2000);
+
+        for (i = 0; i < (W * H); i++) {
+           setHexaColor(cells[i].cy, cells[i].cx, "#00FF00");
+            startAlgo(i, 0, 1);
+            setHexaColor(cells[i].cy, cells[i].cx, defaultColor);
+        }
+
+
+    alert("Max Possible Cost : " + gMaxCost)
+}
+
+mainProg();
